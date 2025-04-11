3a181c30517c8f12b693954e726eb2f9292ee84f
Gwyneth Ormes <134316348+gormes-EPIC@users.noreply.github.com>
2025-04-11 11:59:34 -0600

    Update run_me script

--- run_me.sh
+++ run_me.sh
@@ -2,16 +2,13 @@
 
 # Set variables
 REPO_DIR="/home/pi/thermometer"
-PYTHON_FILE="controller.py"  # or whatever your script is
+PYTHON_FILE="controller.py"  
 
 # Go to repo
 cd "$REPO_DIR" || exit
 
 # Pull latest changes
-git pull origin main  # or whatever your default branch is
-
-# Activate virtual environment if needed
-# source venv/bin/activate
+git pull origin main 
 
 # Run the Python file
 python3 "$PYTHON_FILE"
