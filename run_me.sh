2ed27954b26a07366c021969f58c78a8f7e9935d
Gwyneth Ormes <134316348+gormes-EPIC@users.noreply.github.com>
2025-04-11 10:45:27 -0600

    change pull command

--- run_me.sh
+++ run_me.sh
@@ -8,7 +8,7 @@ PYTHON_FILE="controller.py"  # or whatever your script is
 cd "$REPO_DIR" || exit
 
 # Pull latest changes
-git pull -u origin main  # or whatever your default branch is
+git pull origin main  # or whatever your default branch is
 
 # Activate virtual environment if needed
 # source venv/bin/activate
